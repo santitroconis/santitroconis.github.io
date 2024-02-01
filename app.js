59a0ad4e2a42330e62ac05ca11b2812e55871e82
Santiago Troconis <santitroconis@gmail.com>
2024-02-01 04:38:59 -0400

    building portfolio

---
+++ app.js
@@ -0,0 +1,15 @@
+var prevScrollPos = window.scrollY;
+const shadowClass = "show-shadow";
+
+window.onscroll = function() {
+const currentScrollPos = window.scrollY;
+const navElement = document.getElementById("nav");
+const isScrollingDown = prevScrollPos < currentScrollPos 
+
+navElement.style.top = isScrollingDown ? "-100px" : "0px"
+
+prevScrollPos = currentScrollPos;
+    const navClass = navElement.classList
+    currentScrollPos < 40 ? navClass.remove(shadowClass) : navClass.add(shadowClass)
+}
+
