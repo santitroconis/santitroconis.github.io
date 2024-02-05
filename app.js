e3565e0084ddb2845963ee28a2d23796d40ad6e9
Santiago Troconis <santitroconis@gmail.com>
2024-02-05 12:22:51 -0400

    fix

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
+function home(){
+    window.scrollTo(0,0);
+}
+
 var prevScrollPos = window.scrollY;
 const shadowClass = "show-shadow";
 
 window.onscroll = function() {
-const currentScrollPos = window.scrollY;
-const navElement = document.getElementById("nav");
+    const currentScrollPos = window.scrollY;
+    const navElement = document.getElementById("nav");
 
-const isScrollingDown = ((window.scrollY >= 1) && prevScrollPos < currentScrollPos )  
+    const isScrollingDown = ((window.scrollY >= 1) && prevScrollPos < currentScrollPos);  
 
-navElement.style.top = isScrollingDown ? "-110px" : "0px"
-
-prevScrollPos = currentScrollPos;
+    navElement.style.top = isScrollingDown ? "-110px" : "0px"
+    prevScrollPos = currentScrollPos;
+    
     const navClass = navElement.classList
     currentScrollPos < 40 ? navClass.remove(shadowClass) : navClass.add(shadowClass)
 }
+
 
 
